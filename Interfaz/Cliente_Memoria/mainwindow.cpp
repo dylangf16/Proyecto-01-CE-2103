6f90762e861798ff69d82b0e36b5205c18f80c35
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2022-04-03 11:00:57 -0600

    Arreglo server

--- Interfaz/Cliente_Memoria/mainwindow.cpp
+++ Interfaz/Cliente_Memoria/mainwindow.cpp
@@ -6,8 +6,10 @@
 #include <fstream>
 #include<stdlib.h>
 #include <cstdlib>
-#include "/home/dylan16/Documents/Datos2/Proyecto01/Proyecto-01-CE-2103/Matriz/Source_Files/Hashmap.cpp"
+//#include "/home/dylan16/Documents/Datos2/Proyecto01/Proyecto-01-CE-2103/Matriz/Source_Files/Hashmap.cpp"
 #include "/home/dylan16/Documents/Datos2/Proyecto01/Proyecto-01-CE-2103/Matriz/Source_Files/matriz_dinamica.cpp"
+#include "/home/dylan16/Documents/Datos2/Proyecto01/Proyecto-01-CE-2103/Servidor/Source_Files/Cliente.cpp"
+//#include "/home/dylan16/Documents/Datos2/Proyecto01/Proyecto-01-CE-2103/Servidor/Source_Files/Server.cpp"
 using namespace std;
 int carta1 = NULL;
 int carta2 = NULL;
@@ -26,7 +28,6 @@ MainWindow::~MainWindow(){
 
 //Boton empezar
 void MainWindow::on_btn_start_clicked(){
-    ui->pushButton_41->setIcon(QPixmap("/home/dylan16/Documents/Datos2/Proyecto01/Proyecto-01-CE-2103/Interfaz/Cliente_Memoria/imagenes/SW_1.jpg"));
 }
 
 //Boton verificar
@@ -34,6 +35,7 @@ void MainWindow::on_btn_verif_clicked(){
 
 }
 
+//Carta 0
 void MainWindow::on_pushButton_0_clicked()
 {
     if(carta1 == NULL){
@@ -46,10 +48,68 @@ void MainWindow::on_pushButton_0_clicked()
     //rellenarMatriz(4,5);
 
     string num = "0";
-    string img = buscar(0,0,num);
-    cout << img << endl;
+    string img = Cliente(0,0,num);
     QString qstr = QString::fromStdString(img);
     ui -> pushButton_0-> setIcon(QIcon(qstr));
+}
 
+void MainWindow::on_pushButton_1_clicked()
+{
+    if(carta1 == NULL){
+        carta1 = 1;
+    }
+    if(carta2 == NULL){
+        carta2 = 1;
+    }
+    //Método para arreglar "dos mains" solo ejecutar, tirar error, y luego comentar y seguir
+    //rellenarMatriz(4,5);
+
+    string num = "1";
+    /*
+    string img = buscar(0,0,num);
+    QString qstr = QString::fromStdString(img);
+    ui -> pushButton_1-> setIcon(QIcon(qstr));
+    */
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    if(carta1 == NULL){
+        carta1 = 2;
+    }
+    if(carta2 == NULL){
+        carta2 = 2;
+    }
+    //Método para arreglar "dos mains" solo ejecutar, tirar error, y luego comentar y seguir
+    //rellenarMatriz(4,5);
+
+    string num = "2";
+    /*
+    string img = buscar(0,0,num);
+    QString qstr = QString::fromStdString(img);
+    ui -> pushButton_2-> setIcon(QIcon(qstr));
+    */
+
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    if(carta1 == NULL){
+        carta1 = 3;
+    }
+    if(carta2 == NULL){
+        carta2 = 3;
+    }
+    //Método para arreglar "dos mains" solo ejecutar, tirar error, y luego comentar y seguir
+    //rellenarMatriz(4,5);
+
+    string num = "3";
+    /*
+    string img = buscar(0,0,num);
+    QString qstr = QString::fromStdString(img);
+    ui -> pushButton_3-> setIcon(QIcon(qstr));
+    */
 
 }
--- Interfaz/Cliente_Memoria/mainwindow.h
+++ Interfaz/Cliente_Memoria/mainwindow.h
@@ -22,6 +22,12 @@ private slots:
 
     void on_pushButton_0_clicked();
 
+    void on_pushButton_1_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
